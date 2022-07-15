a43fd791283cfbbc4da9c42f5f9f5caf7ffab887
leo <leonardo1317@yahoo.com.br>
2022-07-15 08:31:40 -0300

    Create test CreateRestaurantTest

---
+++ src/test/java/com/br/rank/list/app/usecases/CreateRestaurantTest.java
@@ -0,0 +1,45 @@
+package com.br.rank.list.app.usecases;
+
+import com.br.rank.list.app.repositories.IRestaurantRepository;
+import com.br.rank.list.app.usecases.impl.CreateRestaurant;
+import com.br.rank.list.domains.Restaurant;
+import com.br.rank.list.infra.http.converters.RestaurantConverter;
+import com.br.rank.list.infra.http.jsons.requests.RestaurantRequestJson;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.context.ApplicationEventPublisher;
+import utils.GetMockJson;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CreateRestaurantTest {
+
+    @InjectMocks
+    private CreateRestaurant createRestaurant;
+    @Mock
+    private IRestaurantRepository restaurantRepository;
+    @Mock
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    private final GetMockJson getMockJson = new GetMockJson();
+
+    @Test
+    public void testExecute() {
+        final var restaurantRequest = getMockJson.execute("requests/restaurant-request", RestaurantRequestJson.class);
+        final var restaurant = getMockJson.execute("domains/restaurant-valid", Restaurant.class);
+
+        when(this.restaurantRepository.save(any(Restaurant.class)))
+                .thenReturn(restaurant);
+
+        final var restaurantSave = createRestaurant.execute(RestaurantConverter.toDomain(restaurantRequest));
+        assertNotNull(restaurantSave);
+        verify(applicationEventPublisher).publishEvent(any(Restaurant.class));
+    }
+}
